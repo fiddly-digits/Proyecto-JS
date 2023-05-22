ec5d8a28a9fe9d57ebb7b2dcbad4f61e1af06cf5
Kherzo99 <karlaherzo99@gmail.com>
2023-05-21 20:56:28 -0600

    cambios en routers

--- js/routers.js
+++ js/routers.js
@@ -1,1 +1,35 @@
 // * Aqui trabajan Angel y Kar
+
+const POST_ENDPOINT = 'https://dev-clone-c0a56-default-rtdb.firebaseio.com/posts/.json';
+
+const getAPost = async () => {
+    let response = await fetch (POST_ENDPOINT);
+    let data = await response.json();
+    return data;
+};
+
+const printPost = async () => {
+    let post = await getAPost();
+    console.log(post);
+};
+
+printPost();
+
+const NewPost = async () => {
+let newPost1 = {
+    postBody : "Hola que hace",
+    postDate : Date,
+};
+
+let response = await fetch (POST_ENDPOINT,
+{
+    method:"POST",
+    body: JSON.stringify(NewPost),
+}
+);
+let data = response.json();
+return data;
+};
+
+
+
