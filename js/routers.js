56a64e64ce88240341f15b181523a7836d1f8c33
Karla Herzo ⚡+ <127571238+kherzo99@users.noreply.github.com>
2023-05-23 22:00:51 -0600

    cambios en routers.js

--- js/routers.js
+++ js/routers.js
@@ -1,1 +1,34 @@
 // * Aqui trabajan Angel y Kar
+
+const POST_ENDPOINT = 'https://dev-clone-c0a56-default-rtdb.firebaseio.com/posts/.json';
+
+const getAPost = async () => {
+    let response = await fetch (POST_ENDPOINT);
+    let data = await response.json();
+    return data;
+};
+
+const printPost = async () => {
+    let post = await getAPost();
+    console.log(post);
+};
+
+printPost();
+
+// LEER UN POST
+const NewPost = async () => {
+let newPost1 = {
+    postBody : "Hola que hace",
+    postDate : Date,
+};
+
+let response = await fetch (POST_ENDPOINT,
+{
+    method:"POST",
+    body: JSON.stringify(NewPost),
+}
+);
+let data = response.json();
+return data;
+};
+
