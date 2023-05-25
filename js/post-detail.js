e3fa31293070cb3fe560d0ff70428dfe41b7f5a2
fiddly-digits <rob.crlu@gmail.com>
2023-05-25 17:18:29 -0600

    added several functions

--- js/post-detail.js
+++ js/post-detail.js
@@ -73,3 +73,58 @@ const popoverList = [...popoverTriggerList].map((popoverTriggerEl) => {
     '<a type="button" class="btn btn-secondary" onclick="logout(event)">Log out<a>';
   return popover;
 });
+
+const POST_ENDPOINT =
+  "https://dev-clone-c0a56-default-rtdb.firebaseio.com/posts/";
+
+const getAPost = async (postID) => {
+  let response = await fetch(`${POST_ENDPOINT}${postID}/.json`);
+  let data = await response.json();
+  return data;
+};
+
+const getUser = async (user) => {
+  let response = await fetch(`${BASE_URL}users/${user}/.json`);
+  let data = await response.json();
+  return data;
+};
+
+const fillPostData = async () => {
+  let params = new URLSearchParams(window.location.search);
+  let paramsID = params.get("id");
+  let postObject = await getAPost(paramsID);
+  let { userID, postDate, postTitle, hashtags, postBody, postImg } = postObject;
+  let { first, second, third, fourth } = hashtags;
+  let userObject = await getUser(userID);
+  let { picture, name } = userObject;
+  let usernameElement = document.getElementById("username");
+  let dateElement = document.getElementById("post-date");
+  let titleElement = document.getElementById("post-title");
+  let creatorPictureElement = document.getElementById("post-user-picture");
+  let paragraphElement = document.getElementById("post-content");
+  let mainImgElement = document.getElementById("post-main-img");
+  let firstHashElement = document.getElementById("first-hash");
+  let secondHashElement = document.getElementById("second-hash");
+  let thirdHashElement = document.getElementById("third-hash");
+  let fourthHashElement = document.getElementById("fourth-hash");
+  let publishUsername = document.createTextNode(`${name.first} ${name.last}`);
+  let publishDate = document.createTextNode(postDate);
+  let publishTitle = document.createTextNode(postTitle);
+  let publishBody = document.createTextNode(postBody);
+  let publishFirstHash = document.createTextNode(first);
+  let publishSecondHash = document.createTextNode(second);
+  let publishThirdHash = document.createTextNode(third);
+  let publishFourthHash = document.createTextNode(fourth);
+  mainImgElement.setAttribute("src", postImg);
+  paragraphElement.appendChild(publishBody);
+  firstHashElement.appendChild(publishFirstHash);
+  secondHashElement.appendChild(publishSecondHash);
+  thirdHashElement.appendChild(publishThirdHash);
+  fourthHashElement.appendChild(publishFourthHash);
+  creatorPictureElement.setAttribute("src", picture);
+  usernameElement.appendChild(publishUsername);
+  dateElement.appendChild(publishDate);
+  titleElement.appendChild(publishTitle);
+};
+
+fillPostData();
